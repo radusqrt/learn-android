dbbee483181183230f185d4c9c876d3c5a361f19
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2017-12-02 02:36:17 +0200

    Add ActionBar

---
+++ CrossfadeViews/app/src/main/java/com/example/radus/crossfadeviews/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.radus.crossfadeviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
